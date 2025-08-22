059b2afc5b69e4cd1b5cf9479ce1df7fdb4e9fef
Kook-s <kooks1126@naver.com>
2025-08-22 23:12:29 +0900

    add package

--- src/main/java/kr/handscope/interfaces/controller/AppController.java
+++ src/main/java/kr/handscope/interfaces/controller/AppController.java
@@ -1,4 +1,4 @@
-package kr.handscope.controller;
+package kr.handscope.interfaces.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
