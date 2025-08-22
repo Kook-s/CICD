fda48cdfbcbdfe81986d7fa83d62625d593f73b4
Kook-s <kooks1126@naver.com>
2025-08-22 15:13:59 +0900

    initial commit

---
+++ src/main/java/kr/handscope/NeuroBridgeApplication.java
@@ -0,0 +1,13 @@
+package kr.handscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/handscope/NeuroBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.handscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
