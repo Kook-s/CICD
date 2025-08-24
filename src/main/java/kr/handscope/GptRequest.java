ecc63871eb93de794f0b84be0ddd5021ca232ea3
Junhee <dkfl7190@naver.com>
2025-08-25 02:46:03 +0900

    feat: gpt api 연동

--- src/main/java/kr/handscope/GptRequest.java
+++ src/main/java/kr/handscope/GptRequest.java
@@ -1,14 +1,10 @@
 package kr.handscope;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record GptRequest(
         String model,
-        List<Message> messages,
-        Double temperature,
-        @JsonProperty("max_completion_tokens")
-        Integer maxCompletionTokens
+        List<Message> messages
 ) {
 
 }
--- src/main/java/kr/handscope/GptService.java
+++ src/main/java/kr/handscope/GptService.java
@@ -2,18 +2,13 @@ package kr.handscope;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Service
 public class GptService {
 
     private final WebClient webClient;
-
-    @Value("${openai.api.url}")
-    private String apiUrl;
 
     public GptService(WebClient webClient) {
         this.webClient = webClient;
@@ -25,21 +20,19 @@ public class GptService {
         Message userMessage = new Message("user", question);
 
         GptRequest gptRequest = new GptRequest(
-                "gpt-5-nano",   // gpt model
-                List.of(systemMessage, userMessage),
-                0.5,        // Accuracy ~ Creativity
-                500
+                "gpt-5-mini",   // gpt model
+                List.of(systemMessage, userMessage)
         );
 
         GptResponse gptResponse = webClient.post()
-                                           .uri(apiUrl)
-                                           .body(Mono.just(gptRequest), GptRequest.class)
+                                           .bodyValue(gptRequest)
                                            .retrieve()
                                            .bodyToMono(GptResponse.class)
                                            .block();
 
         return Optional.ofNullable(gptResponse)
                        .flatMap(GptResponse::getFirstMessageContent)
+                       .filter(a -> !a.isEmpty())
                        .orElse("답변 생성 실패");
     }
 }
